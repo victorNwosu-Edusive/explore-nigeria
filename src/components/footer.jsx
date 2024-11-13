8965746a73286d32610a8d22c1d017aaad889e57
Victor Nwosu <nwosuuvictor@gmail.com>
2024-11-13 08:44:02 +0100

    footer.jsx update for mobile

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,18 +6,18 @@ const Footer = () => {
     return (
         <>
 
-        <div className="h-auto p-10 md:p-32 bg-footer-pattern bg-green-800 bg-blend-color-burn bg-no-repeat bg-cover bg-center">
+        <div className="h-auto p-7 md:p-32 bg-footer-pattern bg-green-800 bg-blend-color-burn bg-no-repeat bg-cover bg-center">
         
         
         <div className="md:flex flex justify-between flex-wrap">
         <div className="flex space-x-3 rtl:space-x-reverse opacity-60">
-        <a className='font-heading text-white text-xl md:text-3xl hover:text-amber-400 duration-300' href=''>Explore Nigeria
+        <a className='font-heading text-white text-md md:text-3xl hover:text-amber-400 duration-300' href=''>Explore Nigeria
           </a>
-          <img src={logo} alt="" className="h-9 w-9"  />
+          <img src={logo} alt="" className="h-6 w-6 md:h-9 md:w-9"  />
           </div>
 
         <div className="md:flex md:items-center md:space-x-9 md:rtl:space-x-reverse md:text-white md:font-primary">
-        <p className="space-x-6 flex text-white font-primary text-sm ">© 2024 Explore Nigeria</p>
+        <p className="space-x-6 flex text-white font-primary text-[12px] md:text-sm ">© 2024 Explore Nigeria</p>
         </div>
         
        </div>
